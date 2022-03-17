c115e2dd1ad21ff31f680f5860f508b8885bc84a
sperrsim <simon.sperr@htl-wels.at>
2022-03-17 09:06:15 +0100

    Topic implemented

--- src/model/Topic.java
+++ src/model/Topic.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Topic {
+    private String name;
+    private ArrayList<Article> articles;
+    private ArrayList<Question> questions;
+
+    public Topic(String name, ArrayList<Article> articles, ArrayList<Question> questions)
+    {
+        this.name = name;
+        this.articles = articles;
+        this.questions = questions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Article> getArticles() {
+        return articles;
+    }
+
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
 }
