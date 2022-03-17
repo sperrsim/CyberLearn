f7dbb1a7176fd0f7d7f4f60f89ae907b3037ca82
sperrsim <simon.sperr@htl-wels.at>
2022-03-17 09:05:18 +0100

    Answer implemented

--- src/model/Answer.java
+++ src/model/Answer.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Answer {
+    private String answerText;
+    private boolean right;
+
+    public Answer(String answerText, boolean right)
+    {
+        this.answerText = answerText;
+        this.right = right;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
 }
