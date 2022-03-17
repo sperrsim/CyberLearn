b218fde6821fe335839f25a0a5bb1c357010cf71
sperrsim <simon.sperr@htl-wels.at>
2022-03-17 09:05:32 +0100

    Article implemented

--- src/model/Article.java
+++ src/model/Article.java
@@ -1,4 +1,22 @@
 package model;
 
+import java.net.URL;
+
 public class Article {
+    private String name;
+    private String link;
+
+    public Article(String name, String link)
+    {
+        this.name = name;
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLink() {
+        return link;
+    }
 }
