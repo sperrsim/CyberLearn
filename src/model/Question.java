f517e840d316351eed756156583d19b1c9477ff6
sperrsim <simon.sperr@htl-wels.at>
2022-03-17 09:06:00 +0100

    Question implemented

--- src/model/Question.java
+++ src/model/Question.java
@@ -1,4 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Question {
+    private String questionText;
+    private ArrayList<Answer> answers;
+
+    public Question(String questionText, ArrayList<Answer> answers)
+    {
+        this.questionText = questionText;
+        this.answers = answers;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public ArrayList<Answer> getAnswers() {
+        return answers;
+    }
 }
